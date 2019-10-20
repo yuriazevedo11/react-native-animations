a2ebbdadea041af9b2e5ce4d019613fcddccf527
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-10-20 03:43:35 -0300

    Operações matemáticas

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Animated} from 'react-native';
 
+/**
+ * Animation types: timing, spring (bounce), decay (acceleration)
+ * Math operations: add, subtract, multiply, divide
+ */
+
 export default function App() {
   const [ballY] = useState(new Animated.Value(0));
+  const [ballX] = useState(Animated.divide(ballY, 2)); //
 
   useEffect(() => {
-    Animated.timing(ballY, {
-      toValue: 550,
-      duration: 1000,
+    Animated.decay(ballY, {
+      velocity: 0.5,
     }).start();
   }, [ballY]);
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.ball, {top: ballY}]} />
+      <Animated.View style={[styles.ball, {top: ballY, left: ballX}]} />
     </View>
   );
 }
