5faa6b7f19dcae25e74a47655ecce34925621195
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-10-20 03:55:41 -0300

    Encadeamento

--- src/index.js
+++ src/index.js
@@ -4,17 +4,42 @@ import {View, StyleSheet, Animated} from 'react-native';
 /**
  * Animation types: timing, spring (bounce), decay (acceleration)
  * Math operations: add, subtract, multiply, divide
+ * Chaining: sequence, parallel, stagger (parallel with delay), loop
  */
 
 export default function App() {
   const [ballY] = useState(new Animated.Value(0));
-  const [ballX] = useState(Animated.divide(ballY, 2)); //
+  const [ballX] = useState(new Animated.Value(0));
 
   useEffect(() => {
-    Animated.decay(ballY, {
-      velocity: 0.5,
-    }).start();
-  }, [ballY]);
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(ballY, {
+          toValue: 200,
+          duration: 500,
+        }),
+        Animated.delay(150),
+        Animated.timing(ballX, {
+          toValue: 250,
+          duration: 500,
+        }),
+        Animated.delay(150),
+        Animated.timing(ballY, {
+          toValue: 0,
+          duration: 500,
+        }),
+        Animated.delay(150),
+        Animated.timing(ballX, {
+          toValue: 0,
+          duration: 500,
+        }),
+        Animated.delay(150),
+      ]),
+      {
+        iterations: 2,
+      },
+    ).start();
+  }, [ballX, ballY]);
 
   return (
     <View style={styles.container}>
