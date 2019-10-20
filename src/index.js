f45f5aa0daea38cf354c2415f8ec29e730abfe29
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-10-20 02:53:12 -0300

    Animação timing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Animated} from 'react-native';
 
-export default function src() {
+export default function App() {
+  const [ballY] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(ballY, {
+      toValue: 550,
+      duration: 1000,
+    }).start();
+  }, [ballY]);
+
   return (
-    <View>
-      <Text>React Native Animations</Text>
+    <View style={styles.container}>
+      <Animated.View style={[styles.ball, {top: ballY}]} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  ball: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: '#f00',
+  },
+});
